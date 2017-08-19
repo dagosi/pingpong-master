26d57150b2a698d2169524e4ceaa9681bc0c76c8
Daniel Gomez-Sierra <danielgomezsierra@gmail.com>
2017-08-19 14:41:09 -0500

    Create method that returns all the match for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
+
+  def matches
+    Match.where("player1_id = :user_id OR player2_id = :user_id", user_id: self.id)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,18 @@ require 'rails_helper'
 describe User, type: :model do
   describe "validations" do
   end
+
+  describe "#matches" do
+    subject(:user) { FactoryGirl.create(:user) }
+
+    it "returns matches where the user is the player1" do
+      matches = FactoryGirl.create_list(:match, 3, player1: user)
+      expect(user.matches).to eq(matches)
+    end
+
+    it "returns matches where the user is the player2" do
+      matches = FactoryGirl.create_list(:match, 3, player2: user)
+      expect(user.matches).to eq(matches)
+    end
+  end
 end

d44e867c049dc8363f51cf5a5bb10283feff8766
Daniel Gomez-Sierra <danielgomezsierra@gmail.com>
2017-08-19 17:33:46 -0500

    Show users names instead of emails when creating matches

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
 
+  scope :order_by_name, -> { order(:name) }
+
   def matches
     Match.where("player1_id = :user_id OR player2_id = :user_id", user_id: self.id)
   end
